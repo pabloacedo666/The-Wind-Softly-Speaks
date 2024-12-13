b9b0280b76a0f1ec2197bef8848709dd7bcbcb98
pabloacedo666 <acedopablo.06@gmail.com>
2024-12-13 16:02:26 +0100

    déconnade 2 michel

--- docs/index.js
+++ docs/index.js
@@ -3,9 +3,12 @@ const pablal = document.getElementById('pablal');
 
 
 document.addEventListener('mousemove', (e) => {
-    console.log(e.clientX)
+    // console.log(e.clientX)
     pablal.style.left = e.clientX + 'px'
-    pablal.style.top = e.clientY
-        + 'px'
-
+    pablal.style.top = e.clientY + 'px'
+})
+document.addEventListener('touchmove', (e) => {
+    // console.log(e)
+    pablal.style.left = e.touches[0].clientX + 'px'
+    pablal.style.top = e.touches[0].clientY + 'px'
 })
